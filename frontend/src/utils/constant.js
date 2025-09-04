5a7c029a041acd326183e12d4237dc4517d79b39
pawan0110 <meroy0075@gmail.com>
2025-09-04 22:04:39 +0530

    fix env

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,6 +2,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
-export const JOB_API_END_POINT = "https://job-portal-8luf.onrender.com/api/v1/job";
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
 export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
