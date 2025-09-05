76aed5ffe1612e9d42885e42a7fdb39ff967fe87
pawan0110 <meroy0075@gmail.com>
2025-09-05 19:40:22 +0530

    fixed controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -108,7 +108,7 @@ export const login = async (req, res) => {
         maxAge: 24 * 60 * 60 * 1000, // 1 day
         httpOnly: true,
         sameSite: "none",
-        secure:true
+       secure: process.env.NODE_ENV === "production",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
