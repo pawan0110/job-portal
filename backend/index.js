17f97dc17754a096f41d3a4ba49da16061b8de80
pawan0110 <meroy0075@gmail.com>
2025-10-05 14:50:28 +0530

    correct port

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,8 @@ app.use("/api/v1/application", applicationRoute);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
+app.listen(PORT, '0.0.0.0', async () => {
   await connectDB();
   console.log(`Server running on port ${PORT}`);
 });
+
